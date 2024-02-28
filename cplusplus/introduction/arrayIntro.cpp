ed0668ef1eaf493b7e41654872b615e1692752e9
Adamstudnt <adaluh171@studentmail.oru.se>
2024-02-29 00:29:50 +0100

    fix

--- cplusplus/introduction/arrayIntro.cpp
+++ cplusplus/introduction/arrayIntro.cpp
@@ -1,36 +1,9 @@
 #include <cmath>
 #include <cstdio>
 #include <vector>
-#include <string>
 #include <iostream>
 #include <algorithm>
-#include <cctype>
 using namespace std;
-
-int charToInt(char c) {
-    return c - '0';
-}
-
-int *readIntoArray(int size)
-{
-    int *arr = new int[size];
-    string line;
-    getline(cin, line);
-    int j = 0;
-    for(int i = 0; i < line.size(); i++)
-    {
-        if (isdigit(line[i]))
-        {
-            if(charToInt(line[i]) < 1 || charToInt(line[i]) > 10000)
-            {
-                return 1;
-            }
-            arr[j] = charToInt(line[i]);
-            j++;
-        }
-    } 
-    return arr;   
-}
 
 void printArrayBackwards(int *arr, int size)
 {
@@ -41,16 +14,18 @@ void printArrayBackwards(int *arr, int size)
     
 }
 
+
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    int arraySize;
-    cin>>arraySize;
-    if(arraySize < 1 || arraySize > 1000)
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin>>n;
+    int A[n];
+
+    for (int i = 0; i < n; i++)
     {
-        return 1;
+        cin>>A[i];
     }
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    int* arr = readIntoArray(arraySize);
-    printArrayBackwards(arr, arraySize);
+    printArrayBackwards(A, n);
+
     return 0;
 }
