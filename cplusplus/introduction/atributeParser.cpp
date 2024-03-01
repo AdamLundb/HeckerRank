172f82d7927b3f9d39518270e787eabfb2fa9c27
Adamstudnt <adaluh171@studentmail.oru.se>
2024-03-01 22:26:20 +0100

    Start of parser challenge

---
+++ cplusplus/introduction/atributeParser.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class 
+
+int main() {
+
+    int howManyLines, howManyQueries;
+    cin>>howManyLines>>howManyQueries;
+    cin.ignore(); 
+
+
+    return 0;
+}
+    
+    
